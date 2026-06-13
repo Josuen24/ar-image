f4446c840bd85214d547e063c5d215c196a281bc
Estudiante LTIC <Estudiante@ltic.local>
2026-06-13 11:09:40 -0500

    commit 2

--- src/components/ARScene.jsx
+++ src/components/ARScene.jsx
@@ -1,36 +1,29 @@
 import { useEffect } from "react";
-import "aframe";
 
 export default function ARScene() {
   useEffect(() => {
-    const script = document.createElement("script");
-    script.src =
-      "https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar-nft.js";
-    script.async = true;
-    document.body.appendChild(script);
+    const aframe = document.createElement("script");
+    aframe.src = "https://aframe.io/releases/1.5.0/aframe.min.js";
+    aframe.async = true;
+    document.body.appendChild(aframe);
+
+    const arjs = document.createElement("script");
+    arjs.src =
+      "https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar.js";
+    arjs.async = true;
+    document.body.appendChild(arjs);
+
+    return () => {
+      document.body.removeChild(aframe);
+      document.body.removeChild(arjs);
+    };
   }, []);
 
   return (
-    <a-scene>
-      <a-assets>
-        <a-asset-item id="model" src="/model.glb"></a-asset-item>
-      </a-assets>
-
-      {/* IMAGE TARGET */}
-      <a-nft
-        type="nft"
-        url="/nft/mi-imagen"
-        smooth="true"
-        smoothCount="10"
-        smoothTolerance="0.01"
-        smoothThreshold="5"
-      >
-        <a-entity
-          gltf-model="#model"
-          scale="50 50 50"
-          position="0 0 0"
-        />
-      </a-nft>
+    <a-scene embedded arjs>
+      <a-marker preset="hiro">
+        <a-box position="0 0.5 0" color="red"></a-box>
+      </a-marker>
 
       <a-entity camera></a-entity>
     </a-scene>
